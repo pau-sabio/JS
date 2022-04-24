78d95f7519bbca1260d738a2b337f6f6c15b0693
pau-sabio <pau.sabio@bootcamp-upgrade.com>
2022-04-24 17:07:03 +0200

    Update

---
+++ Iteracion#1/index.js
@@ -0,0 +1,21 @@
+var myFavoriteHero;
+myFavoriteHero = "Hulk";
+
+console.log(myFavoriteHero);
+
+var x;
+x = 50;
+
+console.log(x);
+
+var h;
+var y;
+
+h = 5;
+y = 10;
+
+console.log(h);
+console.log(y);
+
+var z = h + y;
+console.log (z);
